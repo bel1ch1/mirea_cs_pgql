391ccba29b64387879eff1ef4b82a033b7940ac1
AndreyZ <andreyzv2525@gmail.com>
2024-02-20 11:47:27 +0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 # libraries
+import psycopg2
+import PyQt6
+# Подключение к базе данных
+connetction = psycopg2.connect(
+    database="",
+    user="",
+    password="",
+    host="127.0.0.1",
+    port="5432"
+)
 
+
+curs = connetction.cursor()
 
 
 
 # entry point
-if __name__ == "__main":
+#if __name__ == "__main":
